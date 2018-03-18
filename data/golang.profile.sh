27bac02c1f023bdf32badd8757dd466f094a2dfe
Nico Schmoigl <nico@cts-schmoigl.de>
2018-03-18 11:15:40 +0100

    Add debugging probes

--- data/golang.profile.sh
+++ data/golang.profile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+echo "golang profile activated"
 export PATH=$PATH:/usr/lib/go-1.9/bin
--- data/hub.profile.sh
+++ data/hub.profile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+echo "github cli profile activated"
 export PATH=$PATH:/opt/github
